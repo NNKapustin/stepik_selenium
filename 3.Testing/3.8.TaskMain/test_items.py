01f946649e0a367e4758c0c97ba5e7a3b2910bc8
nikita <nikita.n.kapustin@gmail.com>
2023-05-18 11:24:41 +0300

    3.8. assert changed

--- 3.Testing/3.8.TaskMain/test_items.py
+++ 3.Testing/3.8.TaskMain/test_items.py
@@ -6,5 +6,5 @@ from selenium.webdriver.common.by import By
 def test_add_to_basket_lang(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(10)
-    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
-    assert add_to_cart_button.text, 'There is no text on button'
+    assert browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket'), 'Add to basket button is not found'
+
